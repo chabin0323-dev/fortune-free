817d62a2242e74ce9c5ac2f89237e84287c9de35
chabin0323-dev <chabin0323@gmail.com>
2026-03-17 21:09:16 +0900

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+src/pages/CompatibilityFreePage.tsx
 import CompatibilityFreePage from './pages/CompatibilityFreePage';
 
 function HomePage() {
