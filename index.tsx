8ee2a272cfcd5dd024b4e59e426a8b8b1f368e43
chabin0323-dev <chabin0323@gmail.com>
2026-03-17 19:45:28 +0900

    Add files via upload

--- index.tsx
+++ index.tsx
@@ -1,18 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import FreeApp from "./FreeApp";
 
-const rootElement = document.getElementById("root");
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
+const rootElement = document.getElementById('root');
 if (!rootElement) {
-  throw new Error("root element not found");
+  throw new Error("Could not find root element to mount to");
 }
 
-const path = window.location.pathname;
-
-ReactDOM.createRoot(rootElement).render(
+const root = ReactDOM.createRoot(rootElement);
+root.render(
   <React.StrictMode>
-    {path === "/free" ? <FreeApp /> : <App />}
+    <App />
   </React.StrictMode>
 );
